68756f208d15a816913641ff51f7af285f03c662
Edy Segura <edysegura@gmail.com>
2024-06-29 15:58:05 -0300

    feat: js composition

--- js/app.js
+++ js/app.js
@@ -27,4 +27,8 @@ function fancyPrint(text) {
 
 const printerImplementation = fancyPrint;
 
-nameList.forEach(printerImplementation);
+// prettier-ignore
+nameList
+  .sort()
+  .reverse()
+  .forEach(printerImplementation);
